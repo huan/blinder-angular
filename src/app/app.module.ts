43934c9384d24ee1f5f8e10d2459e4e8eab26f8a
weipeng <wpkuang@jizhitech.io>
2018-09-10 13:10:20 +0800

    add material design and update home page with Qrcode as well as adding the first test code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser'
 import { NgModule } from '@angular/core'
 
-import { AppComponent } from './app.component'
+import { AppComponent } from './app.component';
+import {MatButtonModule} from '@angular/material';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
