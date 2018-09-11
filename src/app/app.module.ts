d05dcf53a47a170cd503d9d21acafac1e1527418
weipeng <wpkuang@jizhitech.io>
2018-09-11 14:55:39 +0800

    finish basic login page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core'
 
 import { AppComponent } from './app.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { LoginComponent } from './login/login.component';
 import { PhotoMarkComponent } from './photo-mark/photo-mark.component';
 import { DownloadPhotoComponent } from './download-photo/download-photo.component';
 import {MatButtonModule} from '@angular/material';
@@ -15,7 +14,6 @@ import { HomeComponent } from './home/home.component';
 const appRoutes: Routes = [
    { path: 'photo/originDownload', component: DownloadPhotoComponent },
    { path: 'photo/:id', component: PhotoMarkComponent },
-   { path: 'login', component: LoginComponent },
    { path: '', component: HomeComponent },
   { path: '**', component: PageNotFoundComponent }
 ];
@@ -23,7 +21,6 @@ const appRoutes: Routes = [
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
     PhotoMarkComponent,
     DownloadPhotoComponent,
     PageNotFoundComponent,
